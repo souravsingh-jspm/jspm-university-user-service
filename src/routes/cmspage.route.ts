50646cf54f244079719aa197a008d195db243b41
singhsourav01 <singhsourav0110@gmail.com>
2026-06-08 10:48:52 +0530

    feat: add routes for CMS page management with CRUD operations

--- src/routes/cmspage.route.ts
+++ src/routes/cmspage.route.ts
@@ -6,3 +6,11 @@ const cmsPageController = new CMSPageController();
 const cmsPageRouter = express.Router();
 
 cmsPageRouter.route(API_ENDPOINTS.CMS_PAGE).post(cmsPageController.create);
+
+cmsPageRouter
+  .route(API_ENDPOINTS.CMS_PAGE_BY_ID)
+  .put(cmsPageController.update)
+  .get(cmsPageController.getById)
+  .delete(cmsPageController.delete);
+
+cmsPageRouter.route(API_ENDPOINTS.CMS_PAGES).get(cmsPageController.getAll);
